000cc8b93ab420caf7fc0f41052582b8ebd825ce
fernando duenas <fernando39615@aim.com>
2021-01-20 22:30:06 -0500

    fisrt commit

---
+++ functions.js
@@ -0,0 +1,43 @@
+const identity = function (value) {
+  return value = value;
+};
+const first = function (array, n) {
+  return  n == undefined ? array[0] : array.slice(0, n);
+  // if (n == undefined) {
+  //   return array[n = 0]
+  // } else {
+  //   return array.slice(0, n)
+  // }
+};
+
+const last = function (array, n) {
+  if ( n === undefined) {
+    return array.pop();
+  }  else if (array.length < n) {
+    return array;
+  } else if (n > 0) {
+    return array.slice(n - (n - 1));
+  } else if (n === 0) {
+    return [];
+  }
+};
+const each = function (collection, iterator) {
+  let result = [];
+  for(let i = 0; i < collection.length; i++){
+    result.push(iterator(collection[i]));
+  }
+  console.log('29')
+  console.log(result.findIndex("ant"))
+  return result;
+};
+const indexOf = function (array, target) {};
+const map = function (collection, iterator) {};
+
+module.exports = {
+  identity,
+  first,
+  last,
+  each,
+  indexOf,
+  map,
+};
